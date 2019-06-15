d82d32d4e7847c3c1edcc843a3b74abb0554da10
Jim Driscoll <jim.a.driscoll@gmail.com>
2019-06-15 11:58:58 +0100

    Let the body operate directly on the file

--- lib/Celery/App.php
+++ lib/Celery/App.php
@@ -428,8 +428,7 @@ class App {
      * @param array|null $server_params Mostly for testing
      */
     public function run(bool $silent = false, ?array $server_params = null) {
-        $body = new \Celery\Body();
-        $body->write(file_get_contents("php://input"));
+        $body = new \Celery\Body("php://input");
         $request = (new \Celery\ServerRequest())
             ->withServerParams($server_params ?? $_SERVER)
             ->withUploadedFiles($_FILES)
