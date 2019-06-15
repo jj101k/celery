bbe8a67b73714755d7b5cbd3845a2049f4fcb547
Jim Driscoll <jim.a.driscoll@gmail.com>
2019-06-15 11:39:14 +0100

    Improve file upload test

--- test/08-fileUploadTest.php
+++ test/08-fileUploadTest.php
@@ -12,11 +12,14 @@ class FileUploadTest extends \PHPUnit\Framework\TestCase {
     public function testFileUpload() {
         $a_filename = tempnam(sys_get_temp_dir(), "upload-example");
         $b_filename = tempnam(sys_get_temp_dir(), "upload-example");
+        $d_filename = tempnam(sys_get_temp_dir(), "upload-example");
         $boundary = rand();
         $a_contents = rand() . rand();
         $b_contents = rand();
+        $d_contents = str_repeat(rand(), rand(0, 65535));
         file_put_contents($a_filename, $a_contents);
         file_put_contents($b_filename, $b_contents);
+        file_put_contents($d_filename, $d_contents);
         $r = new \Celery\ServerRequest();
         $r = $r->withServerParams([
             "HTTP_HOST" => "example.org",
@@ -41,6 +44,13 @@ class FileUploadTest extends \PHPUnit\Framework\TestCase {
                     "size" => strlen($b_contents),
                 ],
             ],
+            "d" => [
+                "name" => "foo.png",
+                "type" => "image/png",
+                "tmp_name" => $d_filename,
+                "error" => 0,
+                "size" => strlen($d_contents),
+            ],
         ]);
         $this->assertSame(
             strlen($a_contents),
@@ -57,7 +67,18 @@ class FileUploadTest extends \PHPUnit\Framework\TestCase {
             "" . $r->getUploadedFiles()["a"]->getStream(),
             "getUploadedFiles(): Uploaded file has a stream mode"
         );
+        $d_out = "";
+        $f = $r->getUploadedFiles()["d"]->getStream();
+        while(!$f->eof()) {
+            $d_out .= $f->read(256);
+        }
+        $this->assertSame(
+            $d_contents,
+            $d_out,
+            "getUploadedFiles(): Uploaded file works for streamed read"
+        );
         unlink($a_filename);
         unlink($b_filename);
+        unlink($d_filename);
     }
 }
